a52914c33fae240aad96a94858ba74525b3628aa
Melen Laclais <melen.laclais@etudiant.univ-rennes1.fr>
2024-01-02 16:11:36 +0100

    Frontend finit

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,8 +26,6 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('estAdmin', CheckboxType::class)
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
@@ -37,7 +35,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
